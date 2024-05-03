f4cd327c4ce601e642319b0f26d66977215d6401
Gordon Martin <gordonhughmartin@gmail.com>
2024-05-03 21:24:40 +0100

    mair hings

--- nvim/lua/happy0/lazy.lua
+++ nvim/lua/happy0/lazy.lua
@@ -17,11 +17,14 @@ require("lazy").setup({
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { "scala", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
         })
     end
- }
+ },
+ { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+ { "ThePrimeagen/harpoon" },
+ { "mbbill/undotree" }
 })
