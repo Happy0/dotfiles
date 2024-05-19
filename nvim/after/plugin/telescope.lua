653336e619589d85d537e57f0f9f517f2ef074b6
Gordon Martin <gordonhughmartin@gmail.com>
2024-05-19 13:04:52 +0100

    Configure lsp_find_references harpoon builtin to not show line in file list for readability. Change back to horizontal view.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,14 +3,13 @@ local telescope = require('telescope')
 
 telescope.setup({
   defaults = {
-    --   path_display = { "tail" },
-    layout_strategy = "vertical"
+    pickers = {lsp_references = { show_line = false }}
   }
 })
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>fr', function () builtin.lsp_references({show_line = false}) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files,{})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
