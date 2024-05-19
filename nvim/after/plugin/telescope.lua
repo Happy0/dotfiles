6e7e0d43f4a690f820bf020e706aedb87d16f5cb
Gordon Martin <gordonhughmartin@gmail.com>
2024-05-19 12:38:56 +0100

    Add telescope lsp_references keymap and change layout strategy to vertical to give more room to file paths

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,17 @@
-local builtin = require('telescope.builtin')
+local builtin = require('telescope.builtin') 
+local telescope = require('telescope')
+
+telescope.setup({
+  defaults = {
+    --   path_display = { "tail" },
+    layout_strategy = "vertical"
+  }
+})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files,{})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
