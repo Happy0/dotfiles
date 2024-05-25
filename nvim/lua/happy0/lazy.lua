171bac3a06049e22fd1ee3904415180a954081fc
Gordon Martin <gordonhughmartin@gmail.com>
2024-05-25 16:33:17 +0100

    add flash

--- nvim/lua/happy0/lazy.lua
+++ nvim/lua/happy0/lazy.lua
@@ -45,5 +45,15 @@ require("lazy").setup({
     config = true
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
+  },
+  {
+    'folke/flash.nvim',
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash treesitter" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
+    }
   }
 })
